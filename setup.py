969cb4c64e60521ab8b61c33e051ebe721a17a29
Uchihaobito2010 <paraschourey08@gmail.com>
2026-01-05 22:05:24 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="paras",
+    version="1.0.0",
+    author="YourName",
+    description="Fancy bold text formatter for Python",
+    packages=find_packages(),
+    python_requires=">=3.7",
+)
